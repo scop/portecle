3091a4920e275980b400e9013a3ac7cdff6f1024
Ville Skyttä <ville.skytta@iki.fi>
2004-09-16 20:26:08 +0000

    Add simple (X)Emacs JDEE project file.

---
+++ portecle/prj.el
@@ -0,0 +1,9 @@
+;; Configuration for JDEE (Java Development Environment for Emacs)
+(jde-set-project-name "portecle")
+(jde-set-variables
+ '(jde-db-source-directories (quote ("./src/main")))
+ '(jde-global-classpath (quote ("./build/classes" "./lib/bcprov.jar")))
+ '(jde-run-application-class "net.sf.portecle.FKeyToolGUI")
+ '(c-file-style "stroustrup")
+ ;'(c-file-offsets (quote (inline-open . 0)))
+)
