dbd141d45bb34f59eacad98b01c2d05320ed6dce
Ville Skyttä <ville.skytta@iki.fi>
2004-10-05 22:35:31 +0000

    Bring copyright information up to date.

--- portecle/src/main/net/sf/portecle/crypto/ProviderUtil.java
+++ portecle/src/main/net/sf/portecle/crypto/ProviderUtil.java
@@ -1,14 +1,15 @@
 /*
  * ProviderUtil.java
+ * This file is part of Portecle, a multipurpose keystore and certificate tool.
  *
- * Copyright (C) 2004 Ville Skyttä
+ * Copyright © 2004 Ville Skyttä, ville.skytta@iki.fi
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * (This program is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
