8f016d55cb306f6721aebe9c67fc3e4291c7c11e
Ville Skyttä <ville.skytta@iki.fi>
2004-10-28 19:48:36 +0000

    Avoid *-style imports.

--- portecle/src/main/net/sf/portecle/DGetAlias.java
+++ portecle/src/main/net/sf/portecle/DGetAlias.java
@@ -21,12 +21,26 @@
 
 package net.sf.portecle;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ResourceBundle;
 
-import javax.swing.*;
-import javax.swing.border.*;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.border.EmptyBorder;
 
 /**
  * Dialog used for entering a KeyStore alias.
