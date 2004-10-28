e9fe313b966c25edab65ecddf95abd41da16b78d
Ville Skyttä <ville.skytta@iki.fi>
2004-10-28 19:37:40 +0000

    Avoid *-style imports.

--- portecle/src/main/net/sf/portecle/DGenerateKeyPair.java
+++ portecle/src/main/net/sf/portecle/DGenerateKeyPair.java
@@ -21,12 +21,31 @@
 
 package net.sf.portecle;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ResourceBundle;
 
-import javax.swing.*;
-import javax.swing.border.*;
+import javax.swing.AbstractAction;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 
 import net.sf.portecle.crypto.KeyPairType;
 
