10882f847d7ff705dd6e3ad1c09b9e29bc4de33c
Ville Skyttä <ville.skytta@iki.fi>
2005-02-18 22:24:51 +0000

    2005.

--- portecle/src/main/net/sf/portecle/crypto/X509Ext.java
+++ portecle/src/main/net/sf/portecle/crypto/X509Ext.java
@@ -3,7 +3,7 @@
  * This file is part of Portecle, a multipurpose keystore and certificate tool.
  *
  * Copyright © 2004 Wayne Grant, waynedgrant@hotmail.com
- *             2004 Ville Skyttä, ville.skytta@iki.fi
+ *             2004-2005 Ville Skyttä, ville.skytta@iki.fi
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
