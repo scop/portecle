7243e0e9b6d01e8922e1beb56c4cc58ad81859f2
Ville Skyttä <ville.skytta@iki.fi>
2006-09-16 19:25:01 +0000

    Copyright update.

--- src/main/net/sf/portecle/gui/password/DChangePassword.java
+++ src/main/net/sf/portecle/gui/password/DChangePassword.java
@@ -3,6 +3,7 @@
  * This file is part of Portecle, a multipurpose keystore and certificate tool.
  *
  * Copyright © 2004 Wayne Grant, waynedgrant@hotmail.com
+ *             2006 Ville Skyttä, ville.skytta@iki.fi
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
