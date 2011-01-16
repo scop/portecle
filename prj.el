d6063da2449055e6e803372b1d78d0ab4b83a114
Ville Skyttä <ville.skytta@iki.fi>
2011-01-17 00:50:36 +0200

    Remove obsolete build targets and JDEE project file.

--- prj.el
+++
@@ -1,9 +0,0 @@
-;; Configuration for JDEE (Java Development Environment for Emacs)
-(jde-set-project-name "portecle")
-(jde-set-variables
- '(jde-db-source-directories (quote ("./src/main")))
- '(jde-global-classpath (quote ("./build/classes" "./lib/bcprov.jar")))
- '(jde-run-application-class "net.sf.portecle.FPortecle")
- '(c-file-style "stroustrup")
- ;'(c-file-offsets (quote (inline-open . 0)))
-)
